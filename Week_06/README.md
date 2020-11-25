60c01a45a07e308de29871a341d0729ec9de8686
Andy <2660889125@qq.com>
2020-11-25 21:50:02 +0800

    ci:第六周作业

--- Week_06/README.md
+++ Week_06/README.md
@@ -1,1 +1,125 @@
-学习笔记
+## 函数编程
+### lambda 表达式的形式
+- 直接返回 
+```
+(a) a + 1
+```
+- 含处理过程
+```
+(a) -> {
+....
+return x;
+}
+```
+
+### function interface 种类
+- Function<T, R> 含有参数和返回值的
+- Consumer<T> 只含有参数
+- Supplier<R> 只有返回值
+
+### 闭包
+- 自由变量：指在函数中使用的，但既不是函数参数也不是函数的局部变量的变量。
+- 闭包：指那些能够访问自由变量的函数。（也就是Lambda表达式的参数是需要final）
+- 科里化：是把接受多个参数的函数变换成接受一个单一参数（最初函数的第一个参数）的函数
+
+### 函数式编程演进
+```
+ Consumer<Integer> c1 = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        };
+
+        Consumer<Integer> c2 = (a) -> {
+            System.out.print(a);
+        };
+        
+        Consumer<Integer> c3 = System.out::print;
+
+```
+
+
+
+## Stream的所有操作 
+eg：如何记忆，我们可以想象为很多的船在长江上游运货，中间段（中间操作）到了三峡大坝，就别过滤（filter）了，必须要按顺序（sorted）排着过大坝，过大坝的时候每个船员都想拍个照片（映射），
+到终点（终止操作）的时候需要先检查（max min）货品是否有人问题，然后卸货（reduce），再重新收集货物（toList），运到上游，不断循环迭代（foreach）
+### 中间操作
+- 过滤
+    - filter
+    - distinct
+    - limit
+    - skip
+- 排序
+    - sorted
+- 映射
+    - map
+    - flatMap
+### 终止操作
+- 检查
+    - max
+    - min
+    - count
+    - findFirst 其返回的是一个Optional<T> 这样可以防止null中断流
+- 归约
+    - reduce 需要传入一个初始值，作为第一次迭代的第一个元素
+- 收集
+    - toList
+    - toMap 需要传入key冲突的处理函数和新建map 的函数
+- 迭代
+    - foreach 它传入的函数是一个consumer固会中断     
+    
+###面向对象的设计原则SOLID
+- S 单一责任原则
+- O 开闭原则
+- L 里氏替换原则
+- I 接口分离原则
+- D 依赖注入   
+
+###GOF23种设计模式
+- 创建型
+- 结构型
+- 行为行   
+
+### 各种平台
+- SaaS 软件即服务
+- PasS platform 平台即服务
+- IaaS 即各种云平台，基础设施即服务
+
+- FaaS Function 函数即服务 API平台
+- DaaS 数据即服务
+
+### 数据库设计三大范式
+- 第一范式所有对列都是原子的 （1 肯定是原子的）
+- 第二范式所有的列不能是对主键部分依赖 （2 即假如主键有两个，不能只依赖于其中一个 ）
+- 第三范式所有的列都和主键直接依赖，不能是传递依赖 （3 假如有三个列，一个是主键，一个列A依赖于主键，一个列B依赖于A 那么不符合）
+
+Catalyst 架构师设计表时候用圆珠笔（原子）不分昼夜（部分）地连线 （依赖）
+
+### mysql 文件分类
+- .frm 表结构定义文件
+- .idb 数据文件
+- binlog binlog
+- ib_logfile0 ib_logfile1  日志文件
+- ib_log.index 日志索引文件
+- ibdata1 在开启共享模式时都存储在一个文件里面 innodb_file_pre_table = 1; 
+
+### Mysql 执行流程
+- 
+
+
+### Mysql执行顺序
+- 表
+    - from
+    - join
+    - on
+- 条件
+    - where
+    - group by
+    - having 
+- 列
+    - select
+- 结果
+    - order by
+    - limit    
+Catalyst： （8765 8 -》 B - 》 表， 7-》t-》条件，6-》列，5-》W-》物（结果））
---
+++ Week_06/表结构.sql
@@ -0,0 +1,82 @@
+﻿# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.39)
+# Database: db1
+# Generation Time: 2020-11-25 13:42:41 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table store_commodity
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `store_commodity`;
+
+CREATE TABLE `store_commodity` (
+  `commodity_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `commodity_name` varchar(20) NOT NULL DEFAULT '',
+  `commodity_price` float NOT NULL,
+  `commodity_stock` int(11) NOT NULL,
+  `commodity_decription` varchar(400) DEFAULT NULL,
+  `commodity_picture` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`commodity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table store_order
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `store_order`;
+
+CREATE TABLE `store_order` (
+  `order_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `commodity_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `shipping_address` varchar(200) NOT NULL DEFAULT '',
+  `reciving_phone` char(11) NOT NULL DEFAULT '',
+  `order_amount` float DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table store_user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `store_user`;
+
+CREATE TABLE `store_user` (
+  `user_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) NOT NULL DEFAULT '',
+  `user_addr` varchar(100) DEFAULT NULL,
+  `user_gender` bit(11) DEFAULT NULL,
+  `user_telephone_number` char(11) DEFAULT NULL,
+  `user_brithday` date DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
